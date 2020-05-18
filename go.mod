c27312db7af55fc10877c75b4733dc8985f9b2f7
Ryan Milkiewicz <ryanm@wolfjawstudios.com>
2020-05-18 10:07:08 -0400

    adding docker and postgres

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module garagesale
 
 go 1.12
+
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.0.0
+	google.golang.org/appengine v1.6.6 // indirect
+)
--- main.go
+++ main.go
@@ -5,16 +5,35 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 
+	// ============================================================================
+	// App Starting
+
 	log.Printf("main : Started")
 	defer log.Println("main : Compleated")
+
+	// ============================================================================
+	// Setup Dependencies
+
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// ============================================================================
+	// Start Api Service
 
 	api := http.Server{
 		Addr:         "localhost:8000",
@@ -23,8 +42,11 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 	}
 
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we dont collect this error.
 	serverErrors := make(chan error, 1)
 
+	// Start the service listening for request
 	go func() {
 		log.Printf("main : API listening on %s", api.Addr)
 		serverErrors <- api.ListenAndServe()
@@ -54,6 +76,22 @@ func main() {
 			log.Fatalf("main : could not stop server gracefully : %v", err)
 		}
 	}
+}
+
+func openDB() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", "disable")
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword("postgres", "postgres"),
+		Host:     "localhost",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
 }
 
 //Product is something we sell
